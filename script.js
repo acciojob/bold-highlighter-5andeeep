007bfc637988c263174923b1f8c2150a7c3c263f
Sandeep <newsandeep03@gmail.com>
2023-05-23 18:43:27 +0530

    ans added

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let boldText = document.querySelectorAll('strong');
 function highlight() {
     //Write your code here
-
+    // console.log("Hovered on element.")
+    for(let i=0; i<boldText.length; i++){
+        boldText[i].style.color = "green";
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+    // removeEventListener(onmouseout, highlight())
+    for(let i=0; i<boldText.length; i++){
+        boldText[i].style.color = "black";
+    }
     
 }
